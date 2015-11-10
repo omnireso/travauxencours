fa026380cd79e5f2ad8da31117ebe0612660df59
yann faurie <omnireso@gmail.com>
2015-11-10 10:54:13 +0100

    Fichier de travail

---
+++ plan.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" lang="en-US" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en-US" prefix="og: http://ogp.me/ns#">
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html lang="fr" prefix="og: http://ogp.me/ns#">
+<!--<![endif]-->
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# profile: http://ogp.me/ns/profile#">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15">
+ <title>Plan du site</title>
+<link rel="icon" href="favicon.ico" />
+<link rel="icon" href="/favicon.ico">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="publisher" href="https://plus.google.com/+PhilippeMorel"/>
+<meta property="og:locale" content="fr_FR" />
+<meta property="og:locale:alternate" content="en_GB" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Peintre" />
+<meta property="og:description" content="Tableaux et peintures" />
+<meta property="og:url" content="http://www.philmorel.com/" />
+<meta property="og:site_name" content="Philippe Morel" />
+<meta property="article:publisher" content="https://www.facebook.com/philmorel" />
+<meta name="twitter:card" content="summary"/>
+<meta name="twitter:description" content="Peintures et tableaux de paysages"/>
+<meta name="twitter:title" content="Peintures d'Auvergne"/>
+<meta name="twitter:site" content="@philmorel"/>
+
+<link rel="next" href="http://www.philmorel.com/tableaux.php" />
+<link rel="canonical" href="http://www.philmorel.com/plan.php" />
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+<!-- <script src="https://github.com/twbs/bootstrap/blob/master/docs/assets/js/ie-emulation-modes-warning.js></script> -->
+
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<link href="philmorel.css" rel="stylesheet" type="text/css">
+
+  </head>
+
+
+  <body id="philmorel">
+
+  <section>
+
+  <header>
+
+      
+                 
+          <div id="logo">
+            <a href="/"><h1>Artiste peintre Philippe Morel</h1>
+                              <img src="logo.jpg" alt="Accueil philmorel.com" width="120" height="118">
+            </a>
+          </div>
+          </header>
+          
+          </section>
+          
+          <nav>
+
+
+              <ul><li><a href="http://www.philmorel.com/philippe-morel.php" hreflang="fr">A propos</a>
+                   </li>
+	                 <li><a href="http://www.philmorel.com/tableaux.php" hreflang="fr" title="galerie de tableaux" class="active">Tableaux et peintures</a></li>
+	     	            <li><a href="http://www.philmorel.com/actu.php" hreflang="fr" title="expositions passées et à venir">Actualité et expositions</a></li>
+		                <li><a href="http://www.philmorel.com/presse.php" hreflang="fr" title="article de presse et récompenses">Revue de presse</a></li>
+                    <li><a href="http://www.philmorel.com/contact.php" hreflang="fr" title="formulaire de contact">Contactez Philippe Morel</a></li>
+                     <li><a href="http://www.philmorel.com/livredor.php" hreflang="fr" title="Laissez vos impressions">Livre d'Or</a></li>
+                     <li><a href="http://www.philmorel.com/mentions.php" hreflang="fr" title="éditeur et hebergeur du site et CGU">Mentions légales du site</a></li>
+                   <li><a href="http://www.philmorel.com/en/" hreflang="en" title="version anglaise">English Version</a></li></ul>
+
+
+
+        </nav>
+
+
+ <main>
+
+<section>
+
+<article>
+
+ <h2><a href="plan.php">Plan</a></h2>
+
+
+
+
+<ul>
+
+
+
+
+<li><a href="http://www.philmorel.com/philippe-morel.php" hreflang="fr">A propos du peintre Philippe Morel</a></li>
+<li><a href="http://www.philmorel.com/peintures-paysages.php" hreflang="fr">Peintures de paysages</a></li>
+
+<li><a href="http://www.philmorel.com/tableaux.php" hreflang="fr">Paysages d'Auvergne</a></li>
+
+<li><a href="http://www.philmorel.com/brume-limagne.php" hreflang="fr">Brume sur la Limagne</a></li>
+
+<li><a href="http://www.philmorel.com/chaine-puys.php" hreflang="fr">La chaîne des puys vue du nord</a></li>
+
+<li><a href="http://www.philmorel.com/hameau-dores.php" hreflang="fr">Le hameau des Dores</a></li>
+
+<li><a href="http://www.philmorel.com/hameau-neige.php" hreflang="fr">Hameau sous la neige</a></li>
+
+<li><a href="http://www.philmorel.com/montpeyroux.php" hreflang="fr">Montpeyroux</a></li>
+
+<li><a href="http://www.philmorel.com/pont-du-chateau.php" hreflang="fr">Pont du Château</a></li>
+
+
+
+
+
+<li><a href="http://www.philmorel.com/tableaux-auvergne.php" hreflang="fr">Tableaux d'Auvergne</a></li>
+
+<!-- TO DO //-->
+
+<li><a href="http://www.philmorel.com/pont-chateau-printemps.php" hreflang="fr">Pont du Château au printemps</a></li>
+
+<li><a href="http://www.philmorel.com/puy-de-come.php" hreflang="fr">Puy de Côme</a></li>
+
+<li><a href="http://www.philmorel.com/puys-come-dome.php" hreflang="fr">Puys de Côme et Dôme</a></li>
+
+
+
+
+<li><a href="http://www.philmorel.com/peintures-paysages-nature.php" hreflang="fr">Paysages de nature</a></li>
+
+<li><a href="http://www.philmorel.com/oree-du-bois.php" hreflang="fr">L'orée du bois</a></li>
+
+<li><a href="http://www.philmorel.com/avant-la-pluie.php" hreflang="fr">Après la pluie</a></li>
+
+<li><a href="http://www.philmorel.com/sous-bois.php" hreflang="fr">Sous-bois</a></li>
+
+<li><a href="http://www.philmorel.com/bord-de-loire.php" hreflang="fr">En bord de Loire</a></li>
+
+<li><a href="http://www.philmorel.com/bords-de-loire.php" hreflang="fr">Les bords de Loire</a></li>
+
+<li><a href="http://www.philmorel.com/riviere.php" hreflang="fr">La rivière</a></li>
+
+
+
+
+
+
+
+<li><a href="http://www.philmorel.com/index.php" hreflang="fr">Accueil</a></li>
+<!-- Index = tableaux.php //-->
+
+
+
+<li><a href="http://www.philmorel.com/tournesols.php" hreflang="fr">Tournesols</a></li>
+
+<li><a href="http://www.philmorel.com/saint-genes-champanelle.php" hreflang="fr">Saint-Genès-Champanelle</a></li>
+
+<li><a href="http://www.philmorel.com/matin-limagne.php" hreflang="fr">Matin en Limagne</a></li>
+
+<li><a href="http://www.philmorel.com/montmorin.php" hreflang="fr">Montmorin</a></li>
+
+<li><a href="http://www.philmorel.com/puy-de-dome.php" hreflang="fr">Puy de Dôme</a></li>
+
+
+
+
+
+<li><a href="http://www.philmorel.com/tableaux-auvergne-paysages.php" hreflang="fr">Tableaux de paysages d'Auvergne</a></li>
+
+
+
+
+<li><a href="http://www.philmorel.com/tableaux-fleurs.php" hreflang="fr">tableaux fleurs</li>
+
+
+
+
+
+
+<li><a href="http://www.philmorel.com/toiles-natures-mortes.php" hreflang="fr">toiles natures mortes</a></li>
+<li><a href="http://www.philmorel.com/agapanthes.php" hreflang="fr">agapanthes</a></li>
+<li><a href="http://www.philmorel.com/anemones.php" hreflang="fr">anémones</li>
+<li><a href="http://www.philmorel.com/bouquet-champetre.php" hreflang="fr">bouquet champetre</a></li>
+<li><a href="http://www.philmorel.com/iris-bleu.php" hreflang="fr">iris-bleu</a></li>
+<li><a href="http://www.philmorel.com/mimosa.php" hreflang="fr">mimosa</a></li>
+<li><a href="http://www.philmorel.com/pommes-et-violettes.php" hreflang="fr">pommes et violettes</a></li>
+<li><a href="http://www.philmorel.com/vigne-vierge.php" hreflang="fr">vigne vierge</a></li>
+
+
+
+
+
+<li><a href="http://www.philmorel.com/tableaux-nus.php" hreflang="fr">Portraits et nus</a></li>
+
+
+
+
+
+
+
+
+<li><a href="http://www.philmorel.com/mentions.php" hreflang="fr">Mentions légales</a></li>
+
+<li><a href="http://www.philmorel.com/contact.php" hreflang="fr">Contact</a></li>
+<li><a href="http://www.philmorel.com/exposition.php" hreflang="fr">Expositions</a></li>
+<li><a href="http://www.philmorel.com/actualite.php" hreflang="fr">Actualité</a></li>
+<li><a href="http://www.philmorel.com/presse.php" hreflang="fr">Revue de presse</a></li>
+<li><a href="http://www.philmorel.com/livre.php" hreflang="fr">Livre d'Or</a></li>
+<li><a href="http://www.philmorel.com/recompenses.php" hreflang="fr">Prix, mentions et récompenses</a></li>
+
+
+
+
+
+
+</ul>
+
+
+
+
+                                
+                
+          
+      </article>
+      
+      
+         
+      
+
+
+<aside></aside>
+
+</section>
+
+</main>
+
+
+
+      <footer>
+      <div id="pied">
+  <p id="breadcrumbs"><a href="http://philmorel.com/" rel="v:url" property="v:title">Artiste peintre</a></p>
+  <p>&copy; Philmorel.com 2015 ce site présente les &#339;uvres en peinture de l'artiste-peintre amateur Philippe Morel, résidant à Chamalières en Auvergne.</p>
+      </div>
+
+     
+  </footer>
+
+ 
+    
+  </body>
+</html>
