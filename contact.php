957b03a03172d9370919be7cb4f74eebce094bae
yann faurie <omnireso@gmail.com>
2015-11-12 14:59:29 +0100

    formulaire de contact

--- contact.php
+++ contact.php
@@ -1,4 +1,37 @@
-<!DOCTYPE html>
+<?php
+$subjectPrefix = '[Contact du Site]';
+$emailTo = 'yann@omnireso.com';
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name    = stripslashes(trim($_POST['form-name']));
+    $email   = stripslashes(trim($_POST['form-email']));
+    $phone   = stripslashes(trim($_POST['form-tel']));
+    $subject = stripslashes(trim($_POST['form-assunto']));
+    $message = stripslashes(trim($_POST['form-mensagem']));
+    $pattern = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
+    if (preg_match($pattern, $name) || preg_match($pattern, $email) || preg_match($pattern, $subject)) {
+        die("Header injection detected");
+    }
+    $emailIsValid = filter_var($email, FILTER_VALIDATE_EMAIL);
+    if($name && $email && $emailIsValid && $subject && $message){
+        $subject = "$subjectPrefix $subject";
+        $body = "Nome: $name <br /> Email: $email <br /> Telefone: $phone <br /> Mensagem: $message";
+        $headers  = "MIME-Version: 1.1" . PHP_EOL;
+        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
+        $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
+        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
+        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
+        mail($emailTo, "=?utf-8?B?".base64_encode($subject)."?=", $body, $headers);
+        $emailSent = true;
+    } else {
+        $hasError = true;
+    }
+}
+?><!DOCTYPE html>
 <!--[if IE 7]>
 <html class="ie ie7" lang="en-US" prefix="og: http://ogp.me/ns#">
 <![endif]-->
@@ -11,10 +44,10 @@
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# profile: http://ogp.me/ns/profile#">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15">
- <title>Revue de presse de l'artiste peintre amateur Philippe Morel</title>
- <meta name="description" content="Retombées presse suite aux expositions ou aux prix et récompenses de Philippe Morel, peintre Chamaliérois.">
+ <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" media="screen">
+ <title>Contactez Philippe Morel artiste peintre amateur Auvergnat</title>
+ <meta name="description" content="Si un des tableaux de la galerie de peinture du peintre amateur Philippe Morel vous intéresse, vous pouvez communiquez avec l'auteur sur cette page.">
 <link rel="icon" href="favicon.ico" />
-<link rel="icon" href="/favicon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="publisher" href="https://plus.google.com/+PhilippeMorel"/>
 <meta property="og:locale" content="fr_FR" />
@@ -33,6 +66,8 @@
 <link rel="next" href="http://www.philmorel.com/actu.php" />
 <link rel="canonical" href="http://www.philmorel.com/presse.php" />
 
+<!-- nom prenom tel email agenda message site //-->
+
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 
@@ -50,6 +85,8 @@
 
 
   <body id="philmorel">
+
+   <div class="jumbotron">
 
   <section>
 
@@ -98,10 +135,7 @@
   
   <h1>Contact</h1>
  
- <p>En attendant le formulaire de contact qui devrait venir dans les prochains jours, vous pouvez contacter Philippe Morel via l'adresse :</p>
- 
- 
- 
+
  
  <script language="JavaScript" type="text/javascript">
   function decode(a) {
@@ -115,7 +149,57 @@
 </script>
 
 <p>(Système de protection antispam)</p>
- 
+
+<?php if(!empty($emailSent)): ?>
+
+ <div class="alert alert-success text-center">Votre message a bien été envoyé, merci ! Nous vous répondrons dans les meilleurs délais.</div> 
+
+
+  <?php else: ?>
+        <?php if(!empty($hasError)): ?>
+        <div class="col-md-5 col-md-offset-4">
+            <div class="alert alert-danger text-center">Le serveur a rencontré un problème et votre formulaire n'a pas pu être envoyé. Merci de ré-essayer ultérieurement.</div>
+        </div>
+        <?php endif; ?>
+
+<div class="col-md-6 col-md-offset-3">
+        <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="contactform" class="form-horizontal" role="form" method="post">
+<p>Vous souhaitez contacter l'artiste peintre amateur Philippe Morel, pour connaître le prix d'un tableau, organiser une exposition, ou pour poser une question liée à la peinture ? Merci de remplir les champs ci-dessous. Les zones de saisies suivies d'une astérisque sont obligatoires.</p>
+
+    
+
+<fieldset>
+  
+<legend>Informations personnelles</legend>
+
+<p><label for="nom">Votre nom : </label>
+<input id="nom" type="text" placeholder="votre nom">
+<label for="prenom">Votre prénom : </label>
+<input id="prenom" type="text" placeholder="votre prenom"></p>
+<p><label for="email">Email <sup>*</sup>: <input type="email" placeholder="email" id="email" required></label>
+
+<label for="tel">Tel : <input type="tel" placeholder="0473102030" id="tel" pattern="^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,5})|(\(?\d{2,6}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$" maxlength="10"></label></p>
+
+<label for="agenda">Votre jour de contact préféré : <input type="date" id="agenda"></label>
+
+<p>Si vous possédez un site et souhaitez m'en informer : <label for="site"><input type="url" name="website" placeholder="http://philmorel.com/" id="site"></label></p>
+
+<textarea name="message" placeholder="Votre message ici" autocomplete="on" wrap="hard" rows="10" cols="50" spellcheck="on" required="1"></textarea>
+
+
+<input type="submit" value="envoyer">
+
+
+</fieldset>
+
+
+    </form>
+        Votre email ne sera pas revendu ou cédé à un tiers.
+        Conformément à la loi informatique et libertés modifiée (voir nos <a href="mentions.php" hreflang="fr">mentions légales</a>), vous avez un droit de...
+
+
+
+
  
  
                 
@@ -137,14 +221,24 @@
 
       <footer>
       <div id="pied">
-  <p id="breadcrumbs"><a href="http://philmorel.com/" rel="v:url" property="v:title">Artiste peintre</a></p>
+  <p id="breadcrumbs"><a href="http://philmorel.com/">Artiste peintre</a></p>
    <p>&copy; Philmorel.com 2015 Recueil des travaux artistiques de Philippe Morel</p>
       </div>
 
      
   </footer>
 
- 
+   <?php endif; ?>
+
+    <!--[if lt IE 9]>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <![endif]-->
+    <!--[if gte IE 9]><!-->
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <!--<![endif]-->
+    <script type="text/javascript" src="contactform.js"></script>
+
+ </div>
     
   </body>
 </html>
