c471ce68218654d54df451e86df5209eadcc446a
yann faurie <omnireso@gmail.com>
2015-11-12 09:19:51 +0100

    Mise à jour avec liens

--- tableaux-fleurs.php
+++ tableaux-fleurs.php
@@ -8,298 +8,119 @@
 <!--[if !(IE 7) | !(IE 8) ]><!-->
 <html lang="fr" prefix="og: http://ogp.me/ns#">
 <!--<![endif]-->
- <head>
- <title>Peintures de natures mortes fleurs et bouquets par Philippe Morel</title>
-<link rel="shortcut icon" href="favicon.ico" />
-<meta charset="utf-8">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# profile: http://ogp.me/ns/profile#">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-15">
+ <title>Peintures de natures mortes fleurs et bouquets par Philippe Morel</title>
+ <meta name="description" content="Natures Mortes et tableaux de fleurs par l'artiste peintre Auvergnat amateur Philippe Morel.">
+<link rel="icon" href="favicon.ico" />
+<link rel="icon" href="/favicon.ico">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Biographie de l'artiste-peintre amateur chamaliérois Philippe Morel.">
-
-
-<link rel="canonical" href="http://www.philmorel.com" />
-<link rel="next" href="http://www.philmorel.com/page/2/" />
 <link rel="publisher" href="https://plus.google.com/+PhilippeMorel"/>
 <meta property="og:locale" content="fr_FR" />
+<meta property="og:locale:alternate" content="en_GB" />
 <meta property="og:type" content="website" />
 <meta property="og:title" content="Peintre" />
-<meta property="og:description" content="Artiste peintre" />
-<meta property="og:url" content="https://photogrist.com" />
+<meta property="og:description" content="Tableaux et peintures" />
+<meta property="og:url" content="http://www.philmorel.com/" />
 <meta property="og:site_name" content="Philippe Morel" />
 <meta property="article:publisher" content="https://www.facebook.com/philmorel" />
-<meta property="og:image" content="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xfa1/t31.0-8/c0.73.851.315/p851x315/10269189_698615636879851_5098754868816642907_o.jpg" />
 <meta name="twitter:card" content="summary"/>
-<meta name="twitter:description" content=""/>
-<meta name="twitter:title" content=""/>
+<meta name="twitter:description" content="Peintures et tableaux de paysages"/>
+<meta name="twitter:title" content="Peintures d'Auvergne"/>
 <meta name="twitter:site" content="@philmorel"/>
-<meta name="twitter:domain" content=""/>
-<meta name="twitter:image:src" content="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xfa1/t31.0-8/c0.73.851.315/p851x315/10269189_698615636879851_5098754868816642907_o.jpg"/>
-<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"WebSite","url":"https:\/\/philmorel.com\/","name":"Philippe Morel artiste peintre","alternateName":"Philmorel","potentialAction":{"@type":"SearchAction","target":"https:\/\/philmorel.com\/?s={search_term}","query-input":"required name=search_term"}}</script>
-<script type='application/ld+json'>{"@context":"http:\/\/schema.org","@type":"Organization","url":"https:\/\/philmorel.com","sameAs":["https:\/\/www.facebook.com\/philmorel","https:\/\/instagram.com\/philmorel\/","https:\/\/plus.google.com\/+philmorel","https:\/\/www.youtube.com\/channel\/philmorel","https:\/\/www.pinterest.com\/philmorel\/","https:\/\/twitter.com\/philmorel"],"name":"Philippe Morel","logo":"http:\/\/philmorel.com\logo.png"}</script>
 
+<link rel="next" href="http://www.philmorel.com/bords-de-loire.php" />
+<link rel="canonical" href="http://www.philmorel.com/sous-bois.php" />
 
-
-<link rel="apple-touch-icon" sizes="57x57" href="icons/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="icons/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="icons/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="icons/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="icons/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="icons/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="icons/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="icons/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="icons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="icons/favicon-96x96.png">
-<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-
-
-
-
-
-<!-- Bootstrap core CSS -->
 
 <link href="css/bootstrap.min.css" rel="stylesheet">
 
+<!--[if lt IE 9]><script src="js/ie8-responsive-file-warning.js"></script><![endif]-->
+<!-- <script src="https://github.com/twbs/bootstrap/blob/master/docs/assets/js/ie-emulation-modes-warning.js></script> -->
 
-
-<!-- Documentation extras -->
-
-<link href="css/docs.min.css" rel="stylesheet">
-
-<!--[if lt IE 9]><script src="../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-<script src="js/ie-emulation-modes-warning.js"></script>
-
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
-<!-- Favicons -->
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-<link rel="icon" href="/favicon.ico">
-
-<!--
-<link rel='stylesheet' id='pluto-main-less-css'  href='css/pluto-main-less.css' type='text/css' media='all' />
-<link rel='stylesheet' id='sbuzz-group-css' href='css/minify-b1-sbuzz.css' type='text/css' media='all' />
-<link rel='stylesheet' id='jquery-ui-style-css'  href='css/jquery-ui.css' type='text/css' media='all' />
-<link rel='stylesheet' id='pluto-google-font-css'  href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700|Open+Sans:300,400,700' type='text/css' media='all' />
-<link rel='stylesheet' id='pluto-style-group-css' href='css/minify-b1-pluto-style.css' type='text/css' media='all' />
--->
-
-
-
-
-<!--
-<script type="text/javascript">
-function disableSelection(e){if(typeof e.onselectstart!="undefined")e.onselectstart=function(){return false};else if(typeof e.style.MozUserSelect!="undefined")e.style.MozUserSelect="none";else e.onmousedown=function(){return false};e.style.cursor="default"}window.onload=function(){disableSelection(document.body)}
-</script>
-
-<script type="text/javascript">
-document.oncontextmenu=function(e){var t=e||window.event;var n=t.target||t.srcElement;if(n.nodeName!="A")return false};
-document.ondragstart=function(){return false};
-</script>
-
-<style type="text/css">
-* : (input, textarea) {
-	-webkit-touch-callout:none;
-	-webkit-user-select:none;
-}
-</style>
-
-<style type="text/css">
-img {
-	-webkit-touch-callout:none;
-	-webkit-user-select:none;
-}
-</style>
-
-<script type="text/javascript">
-window.addEventListener("keydown",function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==67||e.which==70||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){e.preventDefault()}});document.keypress=function(e){if(e.ctrlKey&&(e.which==65||e.which==66||e.which==70||e.which==67||e.which==73||e.which==80||e.which==83||e.which==85||e.which==86)){}return false}
-</script>
-
-<script type="text/javascript">
-document.onkeydown=function(e){e=e||window.event;if(e.keyCode==123||e.keyCode==18){return false}}
-</script>
-
-//-->
-
-
-
-<style type="text/css">
-.menu {float:left;width:200px;height:800px;min-height:800px;}
-.menu ul li {/*float:left;*/list-style:none;margin-bottom:3%;padding-top:2.8em;}
-.menu ul li a {position:relative;transition: color 0.3s linear 0s;z-index:10000000;text-decoration:underline;}
-.menu ul li a:hover {text-shadow:0 0 1px #000;text-decoration:none;}
-.menu ul li a::before {"|";}
-article {padding-left:1%;float:left;}
-/*.tableau-cell:empty*/ .tableau-cell {
-    -moz-outline-radius: 8px;
-    outline: 2px solid #000;
-    z-index:100;
-}
-.tableau-cell {
-    background-color: rgba(255, 255, 255, 0.2);
-    border-radius: 8px;
-}
-.tableau-cell, .tableau-intro-cell, .tableau-intro-cell-hover {
-    display: -moz-box;
-    height: 290px;
-    margin: 20px 10px 35px;
-    width: 200px;
-}
-#tableau-grid {
-    -moz-box-flex: 5;
-    overflow: hidden;
-    text-align: center;
-    transition: opacity 100ms ease-out 0s;
-    }
-    
-    img {z-index:50}
-  /*  .main-content-w {width:1200px;height:350px;} */
-  
-  footer {position:absolute;bottom:0;width:100%;text-align:center}
-  
-  .logo {padding-left:1em}
-
-</style>
-
+<link href="philmorel.css" rel="stylesheet" type="text/css">
 
   </head>
-  <body class="home blog menu-position-top menu-style-v2 menu-fixed menu-trigger-hover no-sidebar not-wrapped-widgets fixed-height-index-posts with-infinite-scroll page-fluid-width">
-  
-   <div class="menu">
-                  <div class="menu-inner-w">
-          <div class="logo">
-            <a href="http://www.philmorel.com/">
-                              <img src="logo.jpg" alt="">
-                                        </a>
+
+
+  <body id="philmorel">
+
+  <section>
+
+  <header>
+
+      
+                 
+          <div id="logo">
+            <a href="/">
+                              <img src="logo.jpg" alt="Accueil philmorel.com" width="120" height="118">
+            </a>
           </div>
+          </header>
           
+          </section>
           
-          <div class="menu"><ul><li id="apropos" class=""><a href="http://www.philmorel.com/philippe-morel.php" hreflang="fr">A propos</a>
-</li>
-	<li id="tableaux" class=""><a href="http://www.philmorel.com/tableaux.php" hreflang="fr">Tableaux et peintures</a></li>
-		<li id="actu" class=""><a href="http://www.philmorel.com/actu.php" hreflang="fr"><b>Actualité et expositions</b></a></li>
-		<li id="presse" class=""><a href="http://www.philmorel.com/presse.php" hreflang="fr">Revue de presse</a></li>
-<li id="contact" class=""><a href="http://www.philmorel.com/contact.php" hreflang="fr">Contactez Philippe Morel</a></li>
-<li id="contact" class=""><a href="http://www.philmorel.com/contact.php" hreflang="fr">Livre d'Or</a></li>
-<li id="legal" class=""><a href="http://www.philmorel.com/about/legal.php" hreflang="fr">Mentions légales du site</a></li>
-<li id="legal" class=""><a href="http://www.philmorel.com/EN/" hreflang="en">English Version</a></li>
-</div>
+          <nav>
 
-</div>
 
-</div>
+              <ul><li><a href="http://www.philmorel.com/philippe-morel.php" hreflang="fr">A propos</a>
+                   </li>
+	                 <li><a href="http://www.philmorel.com/tableaux.php" hreflang="fr" title="galerie de tableaux" class="active">Tableaux et peintures</a></li>
+	     	            <li><a href="http://www.philmorel.com/actu.php" hreflang="fr" title="expositions pass�es et � venir">Actualit� et expositions</a></li>
+		                <li><a href="http://www.philmorel.com/presse.php" hreflang="fr" title="article de presse et r�compenses">Revue de presse</a></li>
+                    <li><a href="http://www.philmorel.com/contact.php" hreflang="fr" title="formulaire de contact">Contactez Philippe Morel</a></li>
+                     <li><a href="http://www.philmorel.com/livredor.php" hreflang="fr" title="Laissez vos impressions">Livre d'Or</a></li>
+                     <li><a href="http://www.philmorel.com/mentions.php" hreflang="fr" title="�diteur et hebergeur du site et CGU">Mentions l�gales du site</a></li>
+                   <li><a href="http://www.philmorel.com/en/" hreflang="en" title="version anglaise">English Version</a></li></ul>
 
-  <div class="main-content-w">
- <h1 class="post-title entry-title"><a href="">Tableaux</a></h1>
-  <p>Vous pouvez accéder à l'ensemble de l'oeuvre de Philippe Morel en choissant de consulter ses tableaux par catégorie : <a href="paysages-auvergne.php">Paysages d'Auvergne</a> (20/28) - <a href="nature-paysage.php">Nature et autres paysages</a> (10/19) - <a href="natures-mortes.php">Natures mortes</a> (10) - <a href="peinture-abstraite.php">Peinture abstraite</a> - <a href="portraits-nus.php">Portraits et nus</a> (7)</p>
-  
-  
-  <p>Par technique : <a href="peinture-huile.php">peinture à l'huile</a> - <a href="peinture-acrylique.php">peinture acrylique</a> - <a href="peinture-pastel.php">pastel</a> par format : <a href="peinture-orientation-portrait.php">mode portrait</a> - <a href="peinture-orientation-paysage.php">mode paysage</a> ou par <a href="dimensions-tableaux.php">dimensions</a> par date : <a href="tableaux-90-2000.php">90-2000</a> - <a href="tableaux-2000-2010.php">2000-2010</a> - <a href="tableaux-2010-2015.php">2010-2015</a> - <a href="nouveaux-tableaux.php">nouveautés</a> par <a href="expositions-peinture.php">expositions</a> - <a href="distinctions.php">distinctions</a></p>
-  
-  <div class="main-content-i">
-  
 
-                          
-                          
-  <article>
-    <div class="post-body">
-                        
+
+        </nav>
+
+
+ <main>
+
+<section>
+
+
+
+ <h2><a href="tableaux-fleurs.php">Fleurs et Natures mortes</a></h2>
+
+
+<div id="tableau-grid">
+<div class="tableau-cell"><figure><a href="glaieuls.php"><img width="200" height="290" src="fleurs/glaieuls_v.jpg" alt="tableaux de gla�euls" /></a></figure></div><div class="tableau-cell"><figure><a href="bouquet-champetre-2.php"><img width="201" height="290" src="fleurs/bouquet-champetre-2_v.jpg" alt="tableau de bouquet champ�tre" /></a></figure></div><div class="tableau-cell"><figure><a href=""><img width="200" height="290" src="fleurs/iris_v.jpg" alt="" /></a></figure></div></div>
+<div id="tableau-grid">
+<div class="tableau-cell"><figure><a href="iris.php"><img width="290" height="210" src="fleurs/iris_v.jpg" alt="tableau d'iris" /></a></figure></div><div class="tableau-cell"><figure><a href="tableau de renoncules"><img src="fleurs/renoncules_v.jpg" width="200" height="290" alt="" /></a></figure></div><div class="tableau-cell"><figure><a href="tableau de tulipes"><img src="fleurs/tulipes_v.jpg" width="200" height="290" alt="tableau de tulipes" /></a></figure></div></div>
+
       
-              <div class="post-content-body">
-                     
-
-
-<p>Ou bien en vous laissant guider par une visite et en cliquant sur les vignettes des peintures qui vous intéressent :</p>
-
-
-<div id="tableau-grid" style="height: 290px; max-height: 290px; max-width: 1551px;">
-<div class="tableau-cell" id="colza"><figure><a href="automne-auvergne.php"><img width="200" height="290" src="fleurs/glaieuls_v.jpg" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></figure></div><div class="tableau-cell"><figure><a><img width="201" height="290" src="fleurs/bouquet-champetre-2_v.jpg" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></a></figure></div><div class="tableau-cell"><figure><a><img width="200" height="290" src="fleurs/iris_v.jpg" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></figure></div></div>
-<div id="tableau-grid" style="height: 290px; max-height: 290px; max-width: 1551px;">
-<div class="tableau-cell" id="colza"><figure><a><img width="290" height="210" src="fleurs/iris_v.jpg" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></figure></div><div class="tableau-cell"><figure><a><img src="fleurs/renoncules_v.jpg" width="200" height="290" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></figure></div><div class="tableau-cell"><figure><a><img src="fleurs/tulipes_v.jpg" width="200" height="290" class="attachment-pluto-fixed-height wp-post-image" alt="" /></a></figure></div></div>
-
-                              <p>Acccédez à <a href="tableaux-auvergne.php">Tableaux d'Auvergne</a> page 2</p>
-                                
-                  </div>
-          </div>
-      </article>
+         
       
-      
-      
-      
-      
-</div>                    
-
-</div>
 
 
-      <footer class="site-footer" role="contentinfo">
-    <div class="site-info">
-      <div class="site-footer-i">
-<p id="breadcrumbs"><span xmlns:v="http://rdf.data-vocabulary.org/#"><span typeof="v:Breadcrumb"><a href="https://philmorel.com" rel="v:url" property="v:title">Artiste peintre</a></span></p><br/>
-   <h3>© Philippe Morel</h3>
+<aside></aside>
 
-Ce site présente les oeuvres en peinture de l'artiste-peintre amateur Philippe Morel, résidant à Chamalières en Auvergne.
+</section>
 
+</main>
+
+
+
+      <footer>
+      <div id="pied">
+  <p id="breadcrumbs"><a href="http://philmorel.com/">Artiste peintre</a></p>
+  <p>&copy; Philmorel.com 2015 galerie d'art virtuelle de Philippe Morel, artiste-peintre amateur d'Auvergne.</p>
       </div>
-    </div>
+
+     
   </footer>
-    </div>
-</div>
 
-<!--
-
-  <a href="#" class="os-back-to-top"></a>
-  <div class="display-type"></div>
-  
-  
-<style>#leFooter-content #execphp-3{
-				width:870px;
-			}#leFooter #leFooter-inner { width:960px; }</style><script type="text/javascript">
-	jQuery(document).ready(function($){
-		$('div#leFooter').jleFooter({easing : "easeInOutQuad",animSpeed : 350});
-
-	});
-</script><div id="leFooter">
-	<div class="le-opener-container">
-		<div class="le-opener position-center">
-			Toggle This
-		</div>
-	</div>
-	<div id="leFooter-content">
-		<div id="leFooter-inner">
-		<li id="execphp-3" class="widget widget_execphp">			<div class="execphpwidget">Pied de page</div>
-		</li>		<div style="clear:both;"></div>
-		-->
-		</div>
-	</div>
-</div>
-
-<!--
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-
-
-  <script src="js/bootstrap.min.js"></script>
-
-
-
-  <script src="js/docs.min.js"></script>
-
-
-
-
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="js/ie10-viewport-bug-workaround.js"></script>
-
-
-
-
-
+ 
+    
   </body>
 </html>
